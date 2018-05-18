2917ec0774aa963981c4ac1d9d57e3a4450be0ef
TSN Student <student@ulem.org>
2018-05-18 11:28:18 -0400

    widget client service and all data models complete

--- src/app/services/page.service.client.ts
+++ src/app/services/page.service.client.ts
@@ -1,25 +1,27 @@
 
+import { Injectable } from '@angular/core';
+
+// injecting service into module
+@Injectable()
+
+export class PageService {
+
+  constructor() { }
   	
-	 var pages = [
+	 pages = [
 	  { _id: "321", name: "Post 1", websiteId: "456", description: "Lorem" },
 	  { _id: "432", name: "Post 2", websiteId: "456", description: "Lorem" },
 	  { _id: "543", name: "Post 3", websiteId: "456", description: "Lorem" }
 	]
-
-
 	//  adds the page parameter instance to the local pages array. The new page's websiteId is set to the websiteId parameter
-	function createPage(websiteId, page){
+	createPage(websiteId : string, page : object){
 		page._id = Math.floor(Math.random() * Math.floor(1000000)).toString();
 		page.websiteId = websiteId;
 		this.pages.push(page);
 		return page;
 	}
-
-	createPage("123", {name: "Post4", description: "Hello"});
-	console.log(pages);
-
 	// retrieves the pages in local pages array whose websiteId matches the parameter
-	function findPageByWebsiteId(websiteId) {
+	findPageByWebsiteId(websiteId : string) {
 		let result = [];
 		for(let i =0; i < this.pages.length;i++){
 			if(this.pages[i].websiteId === websiteId) {
@@ -31,7 +33,7 @@
 	}
 
 	//  retrieves the page in local pages array whose _id matches the pageId parameter
-	function findPageById(pageId) {
+	findPageById(pageId : string) {
 		for(let i= 0;i<this.pages.length;i++) {
 			if(this.pages[i]._id === pageId)  {
 				return this.pages[i];
@@ -40,18 +42,18 @@
 	}
 
 	//  updates the page in local pages array whose _id matches the pageId parameter
-	function updatePage(pageId, page) {
+	updatePage(pageId : string, page: any) {
 		let oldPage = this.findPageById(pageId);
 		const index = this.pages.indexOf(oldPage);
-		this.pages[index].page.name;
+		this.pages[index].name = page.name;
 		this.pages[index].description = page.description;
 	}
 
 	//  removes the page from local pages array whose _id matches the pageId parameter
-	function deletePage(pageId) {
+	deletePage(pageId : string) {
 		let oldPage = this.findPageById(pageId);
 		const index = this.pages.indexOf(oldPage);
-		this.websites.splice(index, 1);
+		this.pages.splice(index, 1);
 	}
 
-
+}
--- src/app/services/user.service.cilent.ts
+++ src/app/services/user.service.cilent.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model.client';
 
 // injecting service into module
 @Injectable()
@@ -7,20 +8,20 @@ export class UserService {
 
   constructor() { }
 
-users = [
+users: User[] = [
 	{_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice@gmail.com"},
 	{_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob@whatever.com"},
 	{_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly@hotmail.com"},
 	{_id: "456", username: "shiyu", password: "shiyu", firstName: "Shiyu", lastName: "Wang", email: "swang@ulem.org"}
 	];
 
-  createUser(user: any) {
+  createUser(user: User) {
    user._id = Math.floor(Math.random() * Math.floor(1000000)).toString();
     this.users.push(user);
     return user;
   }
 
-  findUserById(userId: string) {
+  findUserById(userId: String) {
     for (let x = 0; x < this.users.length; x++) {
       if (this.users[x].username === userId) {  
         return this.users[x]; 
@@ -29,14 +30,14 @@ users = [
   }
 }
 
-  findUserByUsername(username: string) { 
+  findUserByUsername(username: String) { 
   //   for (let x = 0; x < this.users.length; x++) {
   //     if (this.users[x].username === username) {  
   //       return this.users[x]; 
   //     }
   //   }
   // }
-    return this.users.find(function(user) {
+    return this.users.find(function(user: User) {
       return user.username === username;
     })
    }
@@ -50,7 +51,7 @@ users = [
   }
 
 
-  updateUser(userId: string, user) { 
+  updateUser(userId: string, user: User) { 
     var oldUser = this.findUserById(userId);
     var index = this.users.indexOf(oldUser);
 
@@ -64,7 +65,7 @@ users = [
  }
 
 
-  deleteUser(userId) {
+  deleteUser(userId: String) {
     var oldUser = this.findUserById(userId);
     var index = this.users.indexOf(oldUser);
     this.users.splice(index, 1);
