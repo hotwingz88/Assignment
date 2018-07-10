ee478878fbc1fdff9937e8b6018d8953e837909b
TSN Student <student@ulem.org>
2018-07-10 13:22:54 -0400

    add database

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -4,7 +4,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds0000.mlab.com:29004/dasdasds'; // use yours
+  connectionString += '@ds233571.mlab.com:33571/heroku_7l6n6lbc'; // use yours
 }
 
 
